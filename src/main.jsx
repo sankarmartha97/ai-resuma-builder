a32bfb99a5543a3140832c7710ae577f31553888
Sankar Martha <sankarmartha.123@gmail.com>
2024-12-04 01:44:54 +0530

    send

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,14 @@ import Dashboard from './dashboard/index.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = 'pk_test_dW5pZmllZC1hbW9lYmEtNjYuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
 const router = createBrowserRouter([
   {
     
     element: <App/>,
     children:[
-     
       {
         path: '/dashboard',
         element:<Dashboard/>
@@ -26,7 +26,6 @@ const router = createBrowserRouter([
   {
     path: '/',
     element:<Home/>
-     
   },
   {
     path: '/auth/signin',
